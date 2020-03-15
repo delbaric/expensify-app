48889aa0b78fe324b86c763e0329801d358ee1da
Your Name <you@example.com>
2020-03-15 21:25:25 +0200

    All up to part 176 including

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux';
 import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses';
 import selectorExpensesTotal from '../selectors/expenses-total';
+import { Link } from 'react-router-dom';
 
 const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses' ;
   const formmatedExpensesTotal = numeral(expensesTotal / 100).format('$0,00.00');
   
   return (
-    <div>
-      <h1>Viewing {expensesCount} {expenseWord} totalling {formmatedExpensesTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formmatedExpensesTotal}</span></h1>
+        <div className="page-header__action">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
