7ad464f907ba001f9b0bc70a12debb7b32d9b760
Your Name <you@example.com>
2020-03-15 14:43:41 +0200

    Revert the tests things. Added support from app to database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyDMm5kXnDWLmhdHMQzcxNfDpVy_aHg-3iY",
+  authDomain: "expensify-d5b28.firebaseapp.com",
+  databaseURL: "https://expensify-d5b28.firebaseio.com",
+  projectId: "expensify-d5b28",
+  storageBucket: "expensify-d5b28.appspot.com",
+  messagingSenderId: "681864867371",
   appId: "1:681864867371:web:7b16e99659e1b9b0aa446e",
   measurementId: "G-KQ40FMYYPR"
 };
@@ -15,6 +15,9 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 export { firebase, database as default };
+
+
+
 
 // database.ref('expenses').push({
 //   description: 'expnes 1',
