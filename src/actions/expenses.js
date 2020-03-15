eabfe727a5a27d32d869f90027f155a8b2fb6c37
Your Name <you@example.com>
2020-03-15 16:19:57 +0200

    added support to fetch data from database before loading site

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -10,11 +10,12 @@ export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const {
       description = '',
-      note = '', 
-      amount = 0, 
+      note = '',
+      amount = 0,
       createdAt = 0
     } = expenseData;
     const expense = { description, note, amount, createdAt };
+
     return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
@@ -24,9 +25,9 @@ export const startAddExpense = (expenseData = {}) => {
   };
 };
 
-export const removeExpense = ( { id } = {}) => ({
+export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
-  id: id
+  id
 });
 
 export const editExpense = (id, updates) => ({
@@ -34,3 +35,25 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
