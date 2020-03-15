8acf79d0a038fd2d2e360684d28f4998ddd047dc
Your Name <you@example.com>
2020-03-15 14:25:57 +0200

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMm5kXnDWLmhdHMQzcxNfDpVy_aHg-3iY",
-  authDomain: "expensify-d5b28.firebaseapp.com",
-  databaseURL: "https://expensify-d5b28.firebaseio.com",
-  projectId: "expensify-d5b28",
-  storageBucket: "expensify-d5b28.appspot.com",
-  messagingSenderId: "681864867371",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:681864867371:web:7b16e99659e1b9b0aa446e",
   measurementId: "G-KQ40FMYYPR"
 };
@@ -14,18 +14,25 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Chen Delbari',
-  age: 26,
-  isMale: true,
-  location: {
-    country: 'Israel',
-    city: 'Beer Sheva'
-  }
-});
+export { firebase, database as default };
 
-database.ref('age').set(27);
-database.ref('attributes').set({
-  height: 170,
-  weight: 63
-})
+// database.ref('expenses').push({
+//   description: 'expnes 1',
+//   note: 'this is my first expense',
+//   amount: 150,
+//   createdAt: 0
+// });
+
+// database.ref('expenses').push({
+//   description: 'second expense',
+//   note: 'this is note',
+//   amount: 100,
+//   createdAt: 100000
+// });
+
+// database.ref('expenses').push({
+//   description: 'number 3',
+//   note: 'this is the final note',
+//   amount: 999.5,
+//   createdAt: 500000
+// });
